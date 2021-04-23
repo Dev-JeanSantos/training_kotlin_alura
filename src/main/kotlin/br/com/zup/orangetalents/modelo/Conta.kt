ccc208a7cb835e05c4dbb9b876d3c84745c24d20
Dev-JeanSantos <jeancbsan@gmail.com>
2021-04-23 09:04:23 -0300

    implementation of global constants and objects

--- src/main/kotlin/br/com/zup/orangetalents/modelo/Conta.kt
+++ src/main/kotlin/br/com/zup/orangetalents/modelo/Conta.kt
@@ -1,5 +1,9 @@
 package br.com.zup.orangetalents.modelo
 
+//Variavel Global pode ser visualizada por todos mas apenas alterada por sua classe
+//var totalContas = 0
+//    private set
+
 abstract class Conta(
     var titular: Cliente,
     var numero: Int
@@ -7,6 +11,25 @@ abstract class Conta(
     var saldo = 0.0
         protected set
 
+    //Object Declarations - Objetos anonimos similares a classes,
+    //onde podemos adicionar properties(atributos) e metodos de classes,
+    //possui nome, e sempre tem que estão em arquivos,  fora do escopo de uma função
+    // o Compainon dá acesso a esse object mesmo estando fora do escopo da classe
+    //Lembrando que ele so faz isso na mesma classe que ele está implementado
+   companion object Contador{
+        var total = 0
+            private set
+    }
+
+    init {
+        println("Criando Conta")
+
+        //foi retirada a variavel global
+        // totalContas++
+
+        //Iremos utilizar object declarations ao inves da variavel global
+        total++
+    }
     fun deposita(valor: Double) {
         println("------Movimentação de Depósito------")
         this.saldo += valor
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,5 +1,37 @@
+import br.com.zup.orangetalents.modelo.*
+
+//Object Declarations - Objetos anonimos similares a classes,
+//onde podemos adicionar properties(atributos) e metodos de classes,
+//possui nome, e sempre tem que estão em arquivos,  fora do escopo de uma função
+
+//object Contador{
+// var total = 0
+//}
+
+
+
 fun main() {
+
+//Object Expressions - Objetos anonimos similares a classes,
+//onde podemos adicionar properties(atributos) e metodos de classes,
+// não possui nome, e sempre tem que estar dento do escopo de uma função
+
+    val  fran = object{
+        val nome: String = "fran"
+        val cpf: String = "987.789.897-87"
+        val senha: Int = 321123
+
+    }
+
+    val paulo = Cliente(nome = "Paulo", cpf = "123.123.123-12", senha = 12345)
+    val meire = Cliente(nome = "Meire", cpf = "123.123.123-12", senha = 12345)
+    val contaPoupancaPaulo = ContaPoupanca(titular = paulo, numero = 1001)
+    val contaCorrentePaulo = ContaCorrente(titular = paulo, numero = 1002)
+    val contaCorrenteMeire = ContaCorrente(titular = meire, numero = 1003)
+
     testaContasDiferentes()
+
+    println("Total de Contas ${Conta.total}")
 }
 
 
