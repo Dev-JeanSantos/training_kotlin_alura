7658573ce0846c0c6ff8abe0205411333fa821da
Dev-JeanSantos <jeancbsan@gmail.com>
2021-04-26 16:30:29 -0300

    Other null safety features

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,15 +1,26 @@
 import br.com.zup.orangetalents.modelo.Endereco
 import br.com.zup.orangetalents.teste.funcao1
+import java.lang.IllegalArgumentException
 
 //*******IMPORTANTE****************
 //Tudo no Kotlin são objetos (Classes)
 
 fun main() {
-   var enderecoNulo: Endereco? = Endereco()
-   //Utilizando a interrogação para evitar NullPointException
+   var enderecoNulo: Endereco? = Endereco(logradouro = "Rua Manoel", complemento = "predio lascado")
+   //Utilizando a interrogação (Safe Call) para evitar NullPointException
    println(enderecoNulo?.logradouro)
+   val logredouroNovo: String? = enderecoNulo?.logradouro
+
    //ou podemos utilizar o let
-   enderecoNulo?.let { endereco: Endereco ->
-      println(endereco.logradouro.length)
+   enderecoNulo?.let {end: Endereco ->
+      println(end.logradouro)
+      println(end.logradouro.length)
+
+      //Elvis Operator => ?:
+      val tamanhoComplemento: Int = end.complemento?.length ?:
+               throw IllegalArgumentException("O Complemento não deve ser vazio!!!")
+      println(tamanhoComplemento)
    }
+
+
 }
