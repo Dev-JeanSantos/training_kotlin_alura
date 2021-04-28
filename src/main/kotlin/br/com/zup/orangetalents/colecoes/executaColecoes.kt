4584e6bcbfbecdcfc9f65dbeac3e1a837e9e346a
Dev-JeanSantos <jeancbsan@gmail.com>
2021-04-28 14:52:43 -0300

    Using the Map

--- src/main/kotlin/br/com/zup/orangetalents/colecoes/executaColecoes.kt
+++ src/main/kotlin/br/com/zup/orangetalents/colecoes/executaColecoes.kt
@@ -2,55 +2,30 @@ package br.com.zup.orangetalents.colecoes
 
 fun main() {
 
-    val assistiramCursoAndroid: MutableSet<String> = mutableSetOf<String>("Jean", "Camilla", "Charles", "Mel", "Fabio")
-    val assistiramCursoLinux: MutableSet<String> = mutableSetOf<String>("Marcos", "Marta", "Jean", "Camilla")
-    val assistiramTodos = mutableSetOf<String>()
+    //Pair -> padrão primitivo caso queirar inserir muitos dados
+    //to -> caso queira inserir muitos dados pode ter problemas de performace
+    val pedidos = mapOf(Pair(1, 20.00), Pair(2, 32.11), 3 to 43.44, 4 to 32.00)
+    println("--------------Impressão Normal--------------------")
+    println(pedidos)
 
-    assistiramTodos.addAll(assistiramCursoAndroid)
-    assistiramTodos.addAll(assistiramCursoLinux)
+    //imprimir o pedido numero 1
+    //impotante utilizar o elvis para evitar nullable
 
-    assistiramTodos.add("Paulo")
-    println("Com Set")
-    println(assistiramTodos)
-    println("----------------------------")
+    println("--------------Apenas um pedido--------------------")
+    val pedido = pedidos[1]
+    pedido?.let {
+        println(pedido)
+    }
 
-    //Operações de conjuntos do SET
-    //Lembrando que a ordem das variaveis influencia as saidas
-    println("Impressão com +")
-    println(assistiramCursoAndroid + assistiramCursoLinux)
-    println("----------------------------")
-
-    println("Impressão com union")
-    println(assistiramCursoAndroid union  assistiramCursoLinux)
-    println("----------------------------")
-
-    println("Impressão com - ")
-    println(assistiramCursoAndroid -  assistiramCursoLinux)
-    println("----------------------------")
-
-    println("Impressão com - inverso")
-    println(assistiramCursoLinux - assistiramCursoAndroid)
-    println("----------------------------")
-
-    println("Impressão com subtract ")
-    println(assistiramCursoAndroid subtract  assistiramCursoLinux)
-    println("----------------------------")
-
-    println("Impressão com intersect ")
-    println(assistiramCursoAndroid intersect assistiramCursoLinux)
-    println("----------------------------")
-
-    val conversaoLista = assistiramTodos.toMutableList()
-    conversaoLista.add("Jean")
-    println("Impressão conversão Set para List")
-    println(conversaoLista)
-    println("Impressão conversão List para Set")
-    println(conversaoLista.toSet())
-
-
-    //val assistiramTodos: Set<String> = assistiramCursoAndroid + assistiramCursoLinux
-
+    //imprimir a lista ordenadamente
+    println("--------------lista Ordenada--------------------")
+    for (pedido in pedidos){
+        println("numero do pedido: ${pedido.key}")
+        println("numero do valor: ${pedido.value}")
+    }
 }
+
+
 
 
 
---
+++ src/main/kotlin/br/com/zup/orangetalents/colecoes/testesSet.kt
@@ -0,0 +1,48 @@
+package br.com.zup.orangetalents.colecoes
+
+fun testesSet() {
+    val assistiramCursoAndroid: MutableSet<String> = mutableSetOf<String>("Jean", "Camilla", "Charles", "Mel", "Fabio")
+    val assistiramCursoLinux: MutableSet<String> = mutableSetOf<String>("Marcos", "Marta", "Jean", "Camilla")
+    val assistiramTodos = mutableSetOf<String>()
+
+    assistiramTodos.addAll(assistiramCursoAndroid)
+    assistiramTodos.addAll(assistiramCursoLinux)
+
+    assistiramTodos.add("Paulo")
+    println("Com Set")
+    println(assistiramTodos)
+    println("----------------------------")
+
+    //Operações de conjuntos do SET
+    //Lembrando que a ordem das variaveis influencia as saidas
+    println("Impressão com +")
+    println(assistiramCursoAndroid + assistiramCursoLinux)
+    println("----------------------------")
+
+    println("Impressão com union")
+    println(assistiramCursoAndroid union assistiramCursoLinux)
+    println("----------------------------")
+
+    println("Impressão com - ")
+    println(assistiramCursoAndroid - assistiramCursoLinux)
+    println("----------------------------")
+
+    println("Impressão com - inverso")
+    println(assistiramCursoLinux - assistiramCursoAndroid)
+    println("----------------------------")
+
+    println("Impressão com subtract ")
+    println(assistiramCursoAndroid subtract assistiramCursoLinux)
+    println("----------------------------")
+
+    println("Impressão com intersect ")
+    println(assistiramCursoAndroid intersect assistiramCursoLinux)
+    println("----------------------------")
+
+    val conversaoLista = assistiramTodos.toMutableList()
+    conversaoLista.add("Jean")
+    println("Impressão conversão Set para List")
+    println(conversaoLista)
+    println("Impressão conversão List para Set")
+    println(conversaoLista.toSet())
+}
