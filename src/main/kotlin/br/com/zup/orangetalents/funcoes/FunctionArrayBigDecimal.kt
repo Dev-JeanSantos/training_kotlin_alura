b46bc08cb027942b00d65ac74509b949ba4be2e2
Dev-JeanSantos <jeancbsan@gmail.com>
2021-04-28 08:39:04 -0300

    Composing operations

--- src/main/kotlin/br/com/zup/orangetalents/funcoes/FunctionArrayBigDecimal.kt
+++ src/main/kotlin/br/com/zup/orangetalents/funcoes/FunctionArrayBigDecimal.kt
@@ -1,6 +1,7 @@
 package br.com.zup.orangetalents.funcoes
 
 import java.math.BigDecimal
+import java.math.RoundingMode
 
 //Criação de uma função array generics
 //vararg => vai receber um grupo de variaveis
@@ -9,5 +10,30 @@ fun bigDecimalArrayOf(vararg valores: String): Array<BigDecimal>{
     return Array<BigDecimal>(valores.size){
         //ocorre a alocação e conversão do dado
             i: Int ->  valores[i].toBigDecimal()
+    }
+}
+
+fun calculaAumentoRelativo(salario: BigDecimal, aumento: BigDecimal) =
+    if (salario < "5000.00".toBigDecimal()) {
+        salario + "500.00".toBigDecimal()
+    } else {
+        (salario * aumento)
+            .setScale(2, RoundingMode.UP)
+    }
+
+//Extension Function
+//reduce irá receber valores e nesse caso irá somar cada incremento
+fun Array<BigDecimal>.somatoria(): BigDecimal{
+    return this.reduce { acumulador, valor ->
+        acumulador + valor
+    }
+}
+
+//Função média com bigDecimal
+fun Array<BigDecimal>.media(): BigDecimal{
+    return if(this.isEmpty()){
+        BigDecimal.ZERO
+    }else{
+        this.somatoria()/size.toBigDecimal()
     }
 }
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,9 +1,12 @@
 import br.com.zup.orangetalents.funcoes.bigDecimalArrayOf
+import br.com.zup.orangetalents.funcoes.calculaAumentoRelativo
+import br.com.zup.orangetalents.funcoes.media
+import br.com.zup.orangetalents.funcoes.somatoria
 import java.math.BigDecimal
 import java.math.RoundingMode
 
 fun main() {
-      val salarios = bigDecimalArrayOf("2500.00", "4500.00", "500.00", "6000.00")
+      val salarios = bigDecimalArrayOf("2500.00", "4500.00", "500.00", "6000.00", "12000.00", "3450.99", "1200.00")
       println("Salarios com a função: ${salarios.contentToString()}")
 
       val aumento = "1.1".toBigDecimal()
@@ -26,22 +29,25 @@ fun main() {
     }
     println("-------------------------------------------")
     println("O Custo salarial nos proximos 6 meses: $salarioPrazo")
+
+    //Novo desafio pegar a média dos 3 maiores salarios do array
+    //Ordenando os salario com o sort
+    val salariosOrdenados = salariosComAumento.sorted()
+    //Função take (pega os res primeiros numeros do array)
+    val tresMenores = salariosOrdenados.take(3)
+    println("-------------------------------------------")
+    println("OS três menores salarios: $tresMenores")
+    //Função takeLast (pega os três ultimos numeros do array/ toTypeArrays converte em array novamente)
+    val tresMaiores: Array<BigDecimal> = salariosOrdenados.takeLast(3).toTypedArray()
+    println("-------------------------------------------")
+    println("OS três maiores salarios: $tresMaiores")
+
+    //Calcular a média dos 3 maiores salarios
+    val media = tresMaiores.media()
+    println("-------------------------------------------")
+    println("A média do 3 maiores salarios: $media")
+
 }
 
-fun calculaAumentoRelativo(salario: BigDecimal, aumento: BigDecimal) =
-    if (salario < "5000.00".toBigDecimal()) {
-        salario + "500.00".toBigDecimal()
-    } else {
-        (salario * aumento)
-            .setScale(2, RoundingMode.UP)
-    }
-
-//Extension Function
-//reduce irá receber valores e nesse caso irá somar cada incremento
-fun Array<BigDecimal>.somatoria(): BigDecimal{
-   return this.reduce { acumulador, valor ->
-      acumulador + valor
-   }
-}
 
 
