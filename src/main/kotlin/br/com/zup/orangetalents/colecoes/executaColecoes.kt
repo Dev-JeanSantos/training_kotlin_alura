012c2a28c4ddd222e9892e4869f25d11b8e650c0
Dev-JeanSantos <jeancbsan@gmail.com>
2021-04-28 14:28:54 -0300

    Set Behaviors

--- src/main/kotlin/br/com/zup/orangetalents/colecoes/executaColecoes.kt
+++ src/main/kotlin/br/com/zup/orangetalents/colecoes/executaColecoes.kt
@@ -12,6 +12,41 @@ fun main() {
     assistiramTodos.add("Paulo")
     println("Com Set")
     println(assistiramTodos)
+    println("----------------------------")
+
+    //Operações de conjuntos do SET
+    //Lembrando que a ordem das variaveis influencia as saidas
+    println("Impressão com +")
+    println(assistiramCursoAndroid + assistiramCursoLinux)
+    println("----------------------------")
+
+    println("Impressão com union")
+    println(assistiramCursoAndroid union  assistiramCursoLinux)
+    println("----------------------------")
+
+    println("Impressão com - ")
+    println(assistiramCursoAndroid -  assistiramCursoLinux)
+    println("----------------------------")
+
+    println("Impressão com - inverso")
+    println(assistiramCursoLinux - assistiramCursoAndroid)
+    println("----------------------------")
+
+    println("Impressão com subtract ")
+    println(assistiramCursoAndroid subtract  assistiramCursoLinux)
+    println("----------------------------")
+
+    println("Impressão com intersect ")
+    println(assistiramCursoAndroid intersect assistiramCursoLinux)
+    println("----------------------------")
+
+    val conversaoLista = assistiramTodos.toMutableList()
+    conversaoLista.add("Jean")
+    println("Impressão conversão Set para List")
+    println(conversaoLista)
+    println("Impressão conversão List para Set")
+    println(conversaoLista.toSet())
+
 
     //val assistiramTodos: Set<String> = assistiramCursoAndroid + assistiramCursoLinux
 
