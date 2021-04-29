12a0c284e6eda97e6916dadf4d6a338e6a97357d
Dev-JeanSantos <jeancbsan@gmail.com>
2021-04-29 10:19:26 -0300

    Type function with parameters and return

--- src/main/kotlin/br/com/zup/orangetalents/functions/executaFunction.kt
+++ src/main/kotlin/br/com/zup/orangetalents/functions/executaFunction.kt
@@ -1,21 +1,44 @@
 package br.com.zup.orangetalents.functions
 
 fun main(){
-    println("*********************Chamada de uma Função************************")
-    val minhaFuncao: () -> Unit = ::teste
-    println(minhaFuncao())
-    println("*********************Chamada de uma Classe************************")
-    val minhaFuncaoClasse: () -> Unit = Teste()
-    println(minhaFuncaoClasse())
+
+    testeTipoFuncaoReferencia()
+    testeTipoFuncaoClasse()
 }
 
-fun teste(){
-    println("Testa a Função")
-}
-
-class Teste : () -> Unit{
-    override fun invoke() {
-        println("Testando a Classe")
+fun testeTipoFuncaoAnonima() {
+    println("*********************Chamada de uma Anônima************************")
+    val minhaFuncaoAnonima: () -> Unit = fun() {
+        println("Minha função Anonima!")
     }
+    println(minhaFuncaoAnonima())
+}
 
+fun testeTipoFuncaoLambda() {
+    println("*********************Chamada de uma Lambda************************")
+    val minhaFuncaoLambda = {
+        println("Minha função Lambda!")
+    }
+    println(minhaFuncaoLambda())
+}
+
+fun testeTipoFuncaoReferencia() {
+    println("*********************Chamada de uma Função************************")
+    val minhaFuncao: (Int, Int) -> Int = ::soma
+    println(minhaFuncao(5, 5))
+}
+fun testeTipoFuncaoClasse() {
+    println("*********************Chamada de uma Classe************************")
+    val minhaFuncaoClasse: (Int, Int) -> Int = Soma()
+    println(minhaFuncaoClasse(10, 30))
+}
+
+fun soma(a: Int, b: Int) : Int{
+    return a + b
+}
+
+class Soma : (Int, Int) -> Int{
+    override fun invoke(p1: Int, p2: Int): Int {
+        return p1 + p2
+    }
 }
