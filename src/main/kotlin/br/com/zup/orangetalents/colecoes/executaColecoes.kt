8bbb5aafef2570a1360bcaec76f4adefd124d95d
Dev-JeanSantos <jeancbsan@gmail.com>
2021-04-29 08:48:51 -0300

    Testing the grouping

--- src/main/kotlin/br/com/zup/orangetalents/colecoes/executaColecoes.kt
+++ src/main/kotlin/br/com/zup/orangetalents/colecoes/executaColecoes.kt
@@ -11,8 +11,20 @@ fun main() {
         Pedido(4, 50.00),
         Pedido(5, 60.00),
     )
+
+    val nomes = listOf(
+        "Alex",
+        "Fran",
+        "Gui",
+        "Ana",
+        "Paulo",
+        "Maria",
+        "Mario",
+        "Gisele"
+    )
+
     pedidos.impressaoFormatada()
-    //Uso dos Associates
+    //Uso dos Associates associate, associateBy e associateWith
     val pedidosMapeados: Map<Int, Pedido> = pedidos.associateBy { pedido -> pedido.numero }
 
     println("******************Imprime todos os pedidos Mapeados******************")
@@ -27,6 +39,30 @@ fun main() {
     val pedidosEspecificoFreteGratis = pedidos.associateWith { pedido -> pedido.valor > 50.00 }
     println("******************Imprime se um pedido em especifico tem frete gratis ******************")
     println(pedidosEspecificoFreteGratis[Pedido(2, valor = 30.00)])
+
+    val mapa: Map<Boolean, Pedido> = pedidos.associateBy { pedido -> pedido.valor > 50.0 }
+    println("******************Imprime um pedido que tem e não tem frete ******************")
+    println(mapa)
+
+    val todosOsPedidosNaoPagamFrete: Map<Boolean, List<Pedido>> = pedidos.groupBy { pedido: Pedido -> pedido.valor > 50.0 }
+    println("******************Imprime todos os nomes dos Pedidos que pagam ou não o frete ******************")
+    println(todosOsPedidosNaoPagamFrete)
+
+    println("******************Imprime todos os nomes dos Pedidos que pagam o frete ******************")
+    println(todosOsPedidosNaoPagamFrete[false])
+
+    println("******************Imprime todos os nomes dos Pedidos que não pagam o frete ******************")
+    println(todosOsPedidosNaoPagamFrete[true])
+
+
+    val agenda: Map<Char, List<String>> = nomes.groupBy { nome ->
+        nome.first()
+    }
+    println("******************Imprime todos as iniciais com os nomes de uma agenda ******************")
+    println(agenda)
+    println("******************Imprime todos nomes que iniciam com a letra A  ******************")
+    println(agenda['A'])
+
 
 }
 
