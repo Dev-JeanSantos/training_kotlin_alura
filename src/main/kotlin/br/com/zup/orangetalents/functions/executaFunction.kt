1fec764f6d3d1af1665853d3b96eaec08a1a4004
Dev-JeanSantos <jeancbsan@gmail.com>
2021-04-30 09:41:01 -0300

    Implementando funções com receptores

--- src/main/kotlin/br/com/zup/orangetalents/functions/executaFunction.kt
+++ src/main/kotlin/br/com/zup/orangetalents/functions/executaFunction.kt
@@ -1,12 +1,32 @@
 package br.com.zup.orangetalents.functions
 
-import br.com.zup.orangetalents.modelo.Cliente
-import br.com.zup.orangetalents.modelo.ContaPoupanca
-import br.com.zup.orangetalents.modelo.Endereco
+import br.com.zup.orangetalents.modelo.Autenticavel
+import br.com.zup.orangetalents.modelo.SistemaInterno
 
-fun main(){
+fun main() {
 
+    somaLambda(30, 40, resultado = {
+        println(it)
+    })
+
+    somaReceiver(10, 30, resultado = {
+        println("Total soma: $this")
+    })
 }
 
 
+fun somaLambda(a: Int, b: Int, resultado: (Int) -> Unit){
+    println("*************Utilizando Lambda*******************")
+    println("Antes da Soma")
+    resultado(a+b)
+    println("Após a Soma")
+}
 
+
+fun somaReceiver(a: Int, b: Int, resultado: Int.() -> Unit) {
+    println("*************Utilizando Receiver*******************")
+    println("Antes da Soma")
+    val valorTotal = a+b
+    resultado(valorTotal)
+    println("Após a Soma")
+}
